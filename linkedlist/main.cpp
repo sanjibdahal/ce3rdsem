27c41a819d7c9ce7d1489940ff985840a921f509
Sanjib Dahal <135316801+sanjibdahal@users.noreply.github.com>
2024-04-15 23:33:39 +0545

    Update main.cpp

--- linkedlist/main.cpp
+++ linkedlist/main.cpp
@@ -9,8 +9,8 @@ int main()
         LinkedList list;
         cout << list.isEmpty() << endl;
         // list.addToHead(34);
-        // list.addToHead(46);
-        // list.addToHead(50);
+        list.addToHead(46);
+        list.addToHead(50);
 
         list.addToTail(55);
         list.addToTail(90);

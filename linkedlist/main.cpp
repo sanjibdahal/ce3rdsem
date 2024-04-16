6aa46128cad55826ea2ec7db6a2dd5e3bed7fa79
Sanjib Dahal <135316801+sanjibdahal@users.noreply.github.com>
2024-04-16 23:43:39 +0545

    Update main.cpp

--- linkedlist/main.cpp
+++ linkedlist/main.cpp
@@ -9,7 +9,7 @@ int main()
         LinkedList list;
         cout << list.isEmpty() << endl;
         // list.addToHead(34);
-        list.addToHead(46);
+        // list.addToHead(46);
         list.addToHead(50);
 
         list.addToTail(55);
